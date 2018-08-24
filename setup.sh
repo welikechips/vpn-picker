ed616bf59e34132f7a814ace76f987a42c946f64
tnory56 <tnorberg@thomasnorberg.com>
2018-08-24 13:17:46 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
+unzip openvpn.zip -d ovpn-files
+cd ovpn-files
+mv ca.rsa.2048.crt ../
+mv crl.rsa.2048.pem ../
+cd ../ 
+#Replace #USERNAME# and #PASSWORD# in credentials.txt with your privateinternetaccess.com acct credentials (Username/Password on separate lines).
+php tools/addcreds.php
+chmod +x ./vpn-picker.sh
+#Copy vpn.desktop to desktop for use.
+ln -s ~/vpn-picker/vpn-picker.sh /usr/local/bin/vpnpicker
